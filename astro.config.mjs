d1566f2e9f9700541b768398819b601bf53806c7
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-10-11 00:28:33 +0800

    added shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react(), tailwind({
+        applyBaseStyles: false,
+      }),],
 });
